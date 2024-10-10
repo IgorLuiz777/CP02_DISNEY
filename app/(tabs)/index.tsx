26a8a69058b6865530abd9e1887e25444251c693
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-10-10 09:33:26 -0300

    feat: adding charactersList and a simple style for the card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,77 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight } from 'react-native';
+
+interface Character {
+  _id: number;
+  films: string[];
+  videoGames: string[];
+  name: string;
+  imageUrl: string;
+  url: string;
+}
 
 export default function HomeScreen() {
+  const [charactersList, setCharactersList] = useState<Character[]>([]);
+
+  async function fetchCharacters() {
+    try {
+      const response = await axios.get('https://api.disneyapi.dev/character');
+      console.log(response.data);
+      setCharactersList(response.data.data);
+    } catch (error) {
+      console.error('Erro ao buscar os personagens: ', error);
+      const response = await axios.get('https://api.disneyapi.dev/character');
+      console.log(charactersList);
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView>
+      <ScrollView>
+        <ThemedView>
+          {(charactersList).map((char) => (
+            <TouchableHighlight style={styles.card} key={char._id}>
+              <>
+                <Text>{char.name}</Text>
+                {char.films.map((films) => {
+                  <Text>{films}</Text>
+                })}
+                <Text>{char.videoGames}</Text>
+                <Image style={styles.image} src={char.imageUrl}></Image>
+                <Text>{char.imageUrl}</Text>
+                <Text>{char.url}</Text>
+              </>
+            </TouchableHighlight>
+          ))}
+        </ThemedView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  card: {
+    marginTop: 25,
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 5,
+    marginVertical: 5,
   },
 });
