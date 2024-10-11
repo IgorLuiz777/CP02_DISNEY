88caf69573bd147a1ac571a03dd1c920dfe9218c
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-10-11 11:11:25 -0300

    feat: adding stack nav, detail and search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,77 +1,23 @@
-import { ThemedView } from '@/components/ThemedView';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/home';
+import DetailScreen from './pages/detail';
 
-interface Character {
-  _id: number;
-  films: string[];
-  videoGames: string[];
-  name: string;
-  imageUrl: string;
-  url: string;
-}
+export type RootStackParamList = {
+  Home: undefined;
+  Detail: { characterId: number };
+};
 
-export default function HomeScreen() {
-  const [charactersList, setCharactersList] = useState<Character[]>([]);
+const Stack = createStackNavigator<RootStackParamList>();
 
-  async function fetchCharacters() {
-    try {
-      const response = await axios.get('https://api.disneyapi.dev/character');
-      console.log(response.data);
-      setCharactersList(response.data.data);
-    } catch (error) {
-      console.error('Erro ao buscar os personagens: ', error);
-      const response = await axios.get('https://api.disneyapi.dev/character');
-      console.log(charactersList);
-    }
-  }
-
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
-
+export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <ThemedView>
-          {(charactersList).map((char) => (
-            <TouchableHighlight style={styles.card} key={char._id}>
-              <>
-                <Text>{char.name}</Text>
-                {char.films.map((films) => {
-                  <Text>{films}</Text>
-                })}
-                <Text>{char.videoGames}</Text>
-                <Image style={styles.image} src={char.imageUrl}></Image>
-                <Text>{char.imageUrl}</Text>
-                <Text>{char.url}</Text>
-              </>
-            </TouchableHighlight>
-          ))}
-        </ThemedView>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Personagens' }} />
+        <Stack.Screen name="Detail" component={DetailScreen} options={{ title: 'Detalhes do Personagem' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    marginTop: 25,
-    margin: 10,
-    padding: 10,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    elevation: 3,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 5,
-    marginVertical: 5,
-  },
-});
